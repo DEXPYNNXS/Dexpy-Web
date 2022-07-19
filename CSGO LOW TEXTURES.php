f5444b623c8d7f7cec6a435504976884ee3e8bfc
DEXPYNNXS <100953410+DEXPYNNXS@users.noreply.github.com>
2022-07-19 18:52:25 -0300

    Delete CSGO LOW TEXTURES.php

--- CSGO LOW TEXTURES.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $pagina = $_SERVER['REQUEST_URI'];
-    $datum = date("d-m-y / H:i:s");
-    $invoegen =  $datum . " - " . $ip . " - " . $pagina . "<br>";
-    $fopen = fopen("ip.html", "a");
-    fwrite($fopen, $invoegen);
-    fclose($fopen);
-    ?>
